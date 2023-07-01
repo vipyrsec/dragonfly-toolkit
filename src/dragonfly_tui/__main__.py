ec65fe41ac8c4cc1c5835dcfcf60633827dd09ef
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-07-01 12:34:15 +0000

    feat(cli): add cli command to spawn the tui
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- src/dragonfly_tui/__main__.py
+++ src/dragonfly_tui/__main__.py
@@ -1,21 +1,4 @@
-from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer
-
-
-class DragonflyToolkitApp(App):
-    """A Textual app to manage stopwatches."""
-
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
-
-    def compose(self) -> ComposeResult:
-        """Create child widgets for the app."""
-        yield Header()
-        yield Footer()
-
-    def action_toggle_dark(self) -> None:
-        """An action to toggle dark mode."""
-        self.dark = not self.dark
-
+from .app import DragonflyToolkitApp
 
 if __name__ == "__main__":
     app = DragonflyToolkitApp()
